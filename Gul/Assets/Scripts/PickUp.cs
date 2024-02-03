5a139b634c92b1a38248ecad6ca265a093d39435
banana21218 <113222030+banana21218@users.noreply.github.com>
2024-02-03 14:52:00 -0800

    Squashed commit of the following:
    commit cf3ddcc779dc7b4d06f8cfe330975b69fe27c339
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Sat Feb 3 14:27:09 2024 -0800
    
        readded input system
    
    commit 3b4e1855ef06250f8d3165cbcdae4af77b27f2b7
    Merge: fcec557 eaffe1a
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Sat Feb 3 14:22:44 2024 -0800
    
        Merge branch 'main' into Arturo
    
    commit fcec557d257c07ebc3006d98f6f49bad53b919e0
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Sat Feb 3 14:06:25 2024 -0800
    
        Update to drop
    
    commit 17236b5ef781f3918224e5a5d5b459f879437455
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Fri Feb 2 22:27:47 2024 -0800
    
        Drop Update
    
    commit ff6dc2402699c5708501056fa57bbc2451c0b276
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Fri Feb 2 20:28:32 2024 -0800
    
        change to trigger
    
    commit 514852d65e858a06425e3a740dd03a8cf488d232
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Fri Feb 2 20:15:15 2024 -0800
    
        Flying and PickUp
    
    commit 24aade84d60d0d4ed27c90406ecee4471ed1d582
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Fri Feb 2 19:13:48 2024 -0800
    
        Folders
    
    commit 0d34692656934be4218cae68127ddac2812bf006
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Fri Feb 2 19:07:01 2024 -0800
    
        Cube Example
    
    commit f26f8489a63706fce2734190366de3128f35bba7
    Author: banana21218 <113222030+banana21218@users.noreply.github.com>
    Date:   Fri Feb 2 18:59:07 2024 -0800
    
        Cube Example

---
+++ Gul/Assets/Scripts/PickUp.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public GameObject holding;
+    public bool Holding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        holding.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            Holding = true;
+            holding.SetActive(true);
+            Destroy(other.gameObject);
+        }
+    }
+}
