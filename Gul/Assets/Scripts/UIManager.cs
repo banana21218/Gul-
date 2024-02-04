8744a764efd6d1e39d522c4454c387acde5811f0
MartinAVP <129893589+MartinAVP@users.noreply.github.com>
2024-02-04 13:22:19 -0800

    PausedMenu

--- Gul/Assets/Scripts/UIManager.cs
+++ Gul/Assets/Scripts/UIManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.InputSystem;
+using UnityEditor;
+using UnityEngine.Android;
 
 public class UIManager : MonoBehaviour
 {
@@ -12,4 +15,60 @@ public class UIManager : MonoBehaviour
         //GameObject.Find("FoodNum");
         foodNum.SetText(food.ToString());
     }
+    //
+    public TMP_Text TimerText;
+    public bool playing = true;
+    private float Timer = 600;
+
+    public bool gamePaused;
+    public GameObject pausedMenu;
+
+    private void Start()
+    {
+        StartCoroutine(timer());
+    }
+
+    private IEnumerator timer()
+    {
+        Timer--;
+        yield return new WaitForSeconds(1);
+        TimerText.text = Timer.ToString() + "s";
+        if (Timer != 0)
+        {
+            StartCoroutine(timer());
+        }
+    }
+
+    public void pause(InputAction.CallbackContext context)
+    {
+        Debug.Log("Paused");
+        if (context.performed)
+        {
+            if(gamePaused == false)
+            {
+                startPause();
+                gamePaused = true;
+            }
+            else
+            {
+                endPause();
+                gamePaused = false;
+            }
+        }
+    }
+
+    private void startPause()
+    {
+        Time.timeScale = 0f;
+        pausedMenu.SetActive(true);
+    }
+
+    public void endPause()
+    {
+        Debug.Log("Unpaused");
+        Time.timeScale = 1.0f;
+        pausedMenu.SetActive(false);
+    }
+
+
 }
